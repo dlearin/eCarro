dd245233040fb043a0ff21a713a9fbf374086f2f
carlos <cfernande1408@gmail.com>
2023-03-06 22:05:18 +0100

    v6

--- scripts/ppal.js
+++ scripts/ppal.js
@@ -187,7 +187,29 @@ function receive(data) {
   log(data, 'in');
 }
 
-// Send data to the connected device
 function send(data) {
-  //
+  data = String(data);
+
+  if (!data || !characteristicCache) {
+    return;
+  }
+
+  data += '\n';
+
+  if (data.length > 20) {
+    let chunks = data.match(/(.|[\r\n]){1,20}/g);
+
+    writeToCharacteristic(characteristicCache, chunks[0]);
+
+    for (let i = 1; i < chunks.length; i++) {
+      setTimeout(() => {
+        writeToCharacteristic(characteristicCache, chunks[i]);
+      }, i * 100);
+    }
+  }
+  else {
+    writeToCharacteristic(characteristicCache, data);
+  }
+
+  log(data, 'out');
 }
